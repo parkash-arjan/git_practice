41729a45831477817dc16f766549eaedfedc14b5
Parkash <32680118+parkash-arjan@users.noreply.github.com>
2018-06-10 19:08:31 -0400

    Create GitHub.java

---
+++ src/main/java/com/fibonacci/git/practice/git/practice/GitHub.java
@@ -0,0 +1,8 @@
+package com.fibonacci.git.practice.git.practice;
+public class GitHub
+{
+    public static void main( String[] args )
+    { 
+        System.out.println( "This file is added from GitHUb directly...." );
+    }
+}
