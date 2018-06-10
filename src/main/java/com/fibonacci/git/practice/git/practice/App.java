080629d21564e93ce061df66dea81af5ac5de948
Parkash Arjan <arjan.parkash@gmail.com>
2018-06-10 17:28:13 -0400

    Iniial add

---
+++ src/main/java/com/fibonacci/git/practice/git/practice/App.java
@@ -0,0 +1,13 @@
+package com.fibonacci.git.practice.git.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Git Practice..." );
+    }
+}
