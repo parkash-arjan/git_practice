91811fc92547c67d96c9b6c1dcb5825d93ffedbd
Parkash Arjan <arjan.parkash@gmail.com>
2018-06-10 21:19:17 -0400

    code fixed

--- src/main/java/com/fibonacci/git/practice/git/practice/GitHub.java
+++ src/main/java/com/fibonacci/git/practice/git/practice/GitHub.java
@@ -1,24 +1,22 @@
 package com.fibonacci.git.practice.git.practice;
-<<<<<<< HEAD
-public class GitHub
-{
+
+public class GitHub {
 	private String stashString;
-   public static void main( String[] args )
-    { 
-        System.out.println( "This file is added from GitHUb directly...." );
-        System.out.println( "Some more changes from Eclipse IDE..." );
-        System.out.println( "Changes from GitHub Web UI..." );
-        System.out.println( "One more change from Eclipse IDE..." );
-        System.out.println( "One more chnage from GitHub Web UI..." );
-    }
-    
-    
-    private  void methodAddedFromGitHubUIAfterStash(){
-    	// this line added from Eclipse IDE , after stash
-    }
+
+	public static void main(String[] args) {
+		System.out.println("This file is added from GitHUb directly....");
+		System.out.println("Some more changes from Eclipse IDE...");
+		System.out.println("Changes from GitHub Web UI...");
+		System.out.println("One more change from Eclipse IDE...");
+		System.out.println("One more chnage from GitHub Web UI...");
+	}
+
+	private void methodAddedFromGitHubUIAfterStash() {
+		// this line added from Eclipse IDE , after stash
+	}
 
 	private void stashMethod() {
 
-	}    
-    
+	}
+
 }
