3715774ee5287586c74f1915a62820823fdb0d94
Parkash Arjan <arjan.parkash@gmail.com>
2018-06-10 20:41:36 -0400

    One more change from Eclipse IDE...

--- src/main/java/com/fibonacci/git/practice/git/practice/GitHub.java
+++ src/main/java/com/fibonacci/git/practice/git/practice/GitHub.java
@@ -6,5 +6,6 @@ public class GitHub
         System.out.println( "This file is added from GitHUb directly...." );
         System.out.println( "Some more changes from Eclipse IDE..." );
         System.out.println( "Changes from GitHub Web UI..." );
+        System.out.println( "One more change from Eclipse IDE..." );
     }
 }
